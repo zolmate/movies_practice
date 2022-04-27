f2383bcc78b69459f0cbfdceacea6adb134277f7
zolmate <zolmate@gmail.com>
2022-04-27 22:29:04 +0200

    Synced full code similar to the "beer" project

---
+++ frontend/public/data_movies.js
@@ -0,0 +1,47 @@
+const movies = {
+	id: "movies",
+	logo: "Meaningful Movies",
+	button: "Watch",
+	cards: [
+		{
+			title: "Youth",
+			sub: "2015",
+			text: "A retired orchestra conductor is on vacation with his daughter and his film director best friend in the Alps when he receives an invitation from Queen Elizabeth II to perform for Prince Philip's birthday."
+		},
+		{
+			title: "American Honey",
+			sub: "2016",
+			text: "A teenage girl with nothing to lose joins a traveling magazine sales crew, and gets caught up in a whirlwind of hard partying, law bending and young love as she criss-crosses the Midwest with a band of misfits."
+		},
+		{
+			title: "Blue is the warmest color",
+			sub: "2013",
+			text: "Adèle's life is changed when she meets Emma, a young woman with blue hair, who will allow her to discover desire and to assert herself as a woman and as an adult. In front of others, Adèle grows, seeks herself, loses herself, and ultimately finds herself through love and loss."
+		},
+		{
+			title: "Moonrise Kingdom",
+			sub: "2012",
+			text: "A pair of young lovers flee their New England town, which causes a local search party to fan out to find them."
+		},
+		{
+			title: "Howl's Moving Castle",
+			sub: "2004",
+			text: "When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle."
+		},
+		{
+			title: "Three Billboards Outside Ebbing, Missouri",
+			sub: "2017",
+			text: "A mother personally challenges the local authorities to solve her daughter's murder when they fail to catch the culprit."
+		},
+		{
+			title: "Loro",
+			sub: "2018",
+			text: "An escort service owner uses his girls to get close to politician/PM and media mogul, Silvio Berlusconi. But in 2006, he's preoccupied with his shaky marriage and losing an election."
+		},
+		{
+			title: "Parasite",
+			sub: "2019",
+			text: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan."
+		}
+	]
+}
---
+++ frontend/public/script.js
@@ -0,0 +1,61 @@
+console.log(movies)
+
+// első lépés: meg kell terveznünk 1 db sör komponenseit
+//ha ez nem függvény lenne, hanem sima string, akkor nem lehetne  beletenni változó értékeket paramméterként (name, company, type)
+// a függvény paramétereinek (name, company, type) bármilyen nevet használhatunk, csak azt következetesen kell használni
+// ${} - template literal
+const movieCardComponent = function (movieTitle, releaseYear, movieAbout){
+    return `
+        <div class="card">
+            <div class="movieTitle">${movieTitle}</div>
+            <div class="releaseDate">${releaseYear}</div>
+            <div class="movieAbout">${movieAbout}</div>
+        </div>
+    `
+}
+// ezek a változó nevek (movieTitle, releaseYear, movieAbout) csak a függvény belsejében érvényesek - azon kívül nem tudunk rájuk hivatkozni
+
+const movieTitleComponent = `<h1>Movies</h1>`;
+// most a movieTitleComponent néven létrehoztunk egy változót, amit fentebb "határoztunk meg"
+// ehelyett az is egy jó megoldás lett volna, ha nem változót hozunk létre argumentumként, hanem a backtick (``) között lévő részt illesztjük be a változó nevének helyére (a rootElement.insertAdjacentHtml beforeend után)
+
+const loadEvent = function() {
+    const rootElement = document.getElementById("root");
+    console.log(rootElement)>
+    rootElement.insertAdjacentHTML("beforeend", movieTitleComponent)
+
+    console.log(movies.cards); //a movies-on belül található tömbre (=array), azon belül cardsra hivatkozás
+    console.log(movies.logo); //a beers-en belül található
+
+    for (const movie of movies.cards) { 
+        //sima for loop
+        //iterator (első érték): tetszőleges érték, itt: movie - még "nem létezik"
+        //object (of után): movies.cards (elérési útvonal)
+        console.log(movie);
+        /*console.log(movies.cards[0].title) //mivel a cards egy tömb, az elérési útvonalba kell a [] - a 0 viszont csak az első indexen lévőt írja ki: Mango Bay*/
+        //ennek a helyére kreáltuk a "beer" iterátort
+        rootElement.insertAdjacentHTML("beforeend", movieCardComponent(movie.title, movie.sub, movie.text)) 
+    }
+}
+
+// Ezzel a ciklussal végiglépkedünk a tömb elemein. Ennek a tömbnek az elérési útvonala a movies nevű változó cards kulcsán lévő tömb. Ezt a folyamatot valahogy el kell nevezni egy "iterátorral", ami bármi lehet. Itt: "movie" (egyesszámban feljebb), amire később hivatkozunk (movie.title, movie.sub, movie.text)
+
+
+// instertAdjacentHTML: ez egy metódus, 2 dolgot kér: az első egy position string, és egy olyan stringet (argumentumot), amit html kóddá tud konvertálni
+// a `` (backtick) utána azért kell, mert többsoros kódot szeretnénk berakni
+
+// for ciklusok (loop):
+// van adat és nem kell számolni: "for...of" loop
+// nincs adat és el kell számolni X-ig (pl.: a naptárnál a hónapok, napok száma) sima "for" loop -> i++
+
+
+window.addEventListener("load", loadEvent)
+console.log(window)
+// oldal betöltődése JS szempontjából: a) a JS létrehoz egy "window" objektumot
+// minden JS scriptben benne kell lennie a window.addEventListener("load", loadEvent)-nek
+//eseményfigyelő (eventlistener) - 
+
+
+// Feladat: for és for of ciklusok átnézése
+// Extra feladat: ugyanilyen adatstruktúrájú "dolog"
+// Új repository-ba ugyanezt próbáljuk megcsinálni - a feladatban mozi filmcímek lesznek
